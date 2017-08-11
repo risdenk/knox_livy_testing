5d561b289187bfcc81d0cccdc1f0e784ee24e315
Kevin Risden <krisden@apache.org>
2017-08-11 09:38:35 -0500

    Add curl commands to test_knox_livy.sh

--- test_knox_livy.sh
+++ test_knox_livy.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 CURL="curl -i -k -u admin:admin-password"
-KNOX="https://localhost:8443/gateway/default/livy"
+KNOX="https://localhost:8443/gateway/default"
 
-#$CURL "$KNOX/"
+$CURL "$KNOX/livy/v1/sessions"
+$CURL "$KNOX/livy/v1/batches"
 
